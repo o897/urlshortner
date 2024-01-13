6e345553aada7fbcdff365aa39bf0750f6476dbb
Replit user <>
2024-01-13 02:30:25 +0200

    remove some comments

--- index.js
+++ index.js
@@ -43,12 +43,9 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
-
-
 
 app.post('/api/shorturl', (req, res) => {
   
